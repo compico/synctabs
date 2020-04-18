1da04b0cc9de9e910ef3c74891ca44c40fb7af30
Compico <compico@mail.ru>
2020-04-19 01:27:39 +0300

    add new features in addtabs menu. update js code update css update popup template

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+; let tooltipElem;
+
 async function addTabForm() {
     ts('#addTabsForm').modal('show');
 }
@@ -6,16 +8,30 @@ async function hideAddTab() {
 }
 
 async function bindval(url = '') {
-    if (isValidURLWithProtocol(url)) {
-        document.getElementById("label-url-popup").className = "ts positive label";
-        document.getElementById("input-url-popup").className = "ts basic labeled success input";
-        document.getElementById("error-url-popup").hidden = true;
-        document.getElementById("loading-form-popup").hidden = false;
-        document.getElementById("pushbtn-url-popup").disabled = false;
+    let label_url_addtab = document.getElementById("label-url-addtab")
+        , input_url_addtab = document.getElementById("input-url-addtab")
+        , loading_form_addtab = document.getElementById("loading-form-addtab")
+        , pushbtn_url_addtab = document.getElementById("pushbtn-url-addtab")
+        , error_url_addtab = document.getElementById("error-url-addtab")
+        , x = isValidURLWithProtocol(url);
+    if (x) {
+        loading_form_addtab.className = "ts loading segment";
+        label_url_addtab.className = "ts positive label";
+        input_url_addtab.className = "ts basic labeled fluid success input";
+        loading_form_addtab.hidden = false;
+        pushbtn_url_addtab.disabled = false;
+        error_url_addtab.innerHTML = "";
         datas = await postData('/getSiteData', { site: url }).then((datas) => {
-            document.getElementById("loading-form-popup").hidden = true;
+            console.log(datas);
+            if (datas.error != "") {
+                error_url_addtab_error(datas.error);
+            };
+            loading_form_addtab.className = "ts segment";
+            template = generate_inner(datas);
+            loading_form_addtab.innerHTML = template;
         });
-    } else {
+
+    } else if (!x || error != '') {
         if (isValidURLWoutProtocol(url)) {
             console.log(url);
             url = "https://" + url;
@@ -23,11 +39,23 @@ async function bindval(url = '') {
             bindval(url);
             return;
         }
-        document.getElementById("label-url-popup").className = "ts negative label";
-        document.getElementById("input-url-popup").className = "ts basic labeled error input";
-        document.getElementById("error-url-popup").hidden = false;
-        document.getElementById("error-url-popup").innerHTML = "Invalid URL";
+        loading_form_addtab.hidden = true;
+        label_url_addtab.className = "ts negative label";
+        input_url_addtab.className = "ts basic labeled fluid error input";
+        error_url_addtab.innerHTML = "Invalid URL";
     };
+}
+
+function error_url_addtab_error(error = '') {
+    let label_url_addtab = document.getElementById("label-url-addtab")
+        , input_url_addtab = document.getElementById("input-url-addtab")
+        , loading_form_addtab = document.getElementById("loading-form-addtab")
+        , error_url_addtab = document.getElementById("error-url-addtab");
+
+    loading_form_addtab.hidden = true;
+    label_url_addtab.className = "ts negative label";
+    input_url_addtab.className = "ts basic labeled fluid error input";
+    error_url_addtab.innerHTML = error;
 }
 
 async function postData(url = '', data = { '': `` }) {
@@ -51,3 +79,16 @@ function isValidURLWoutProtocol(url) {
     var objRE = /^(?:http(s)+:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
     return objRE.test(url);
 }
+//google.com
+function generate_inner(datas) {
+    let template;
+    newline = `<br><br>`
+    div_horisontal_title = `<div class="ts labeled fluid input"><div class="ts basic label">Title:</div> <input type="text" value="` + datas.title + `"></div>`;
+    if (datas.url == "") datas.url = "Add URL..."
+    div_horisontal_url = `<div class="ts labeled fluid input"><div class="ts basic label">URL:</div> <input type="text" value="` + datas.url + `"></div>`;
+    if (datas.description == "") datas.description = "Add description...";
+    div_horisontal_description = `<div class="ts basic label">Description:</div><div class="ts fluid input"><textarea type="text">` + datas.description + `</textarea></div>`;
+    template = div_horisontal_title + newline + div_horisontal_url + newline + div_horisontal_description;
+    return template;
+}
+;
